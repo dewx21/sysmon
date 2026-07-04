3782c2b5be2cf01c593d44c050981990c85b179b
dewx21 <dewanshpsinghwork@gmail.com>
2026-07-04 16:20:43 +0000

    show memory in GB

---
+++ comprehension.py
@@ -0,0 +1,2 @@
+squares = [ n*n for n in [1,2,3,4,5,6,7,8,9,10] if n%2==0]
+print(squares)
--- sysmon.py
+++ sysmon.py
@@ -1,8 +1,8 @@
 try:
     with open("/proc/meminfo") as f:
-        for line in f:
-            if line.startswith("MemTotal"):
-                print(line.strip())
+        mem = {line.split(":")[0]: line.split()[1] for line in f}
+        totalgb=int(mem['MemTotal']) / 1024 / 1024
+        print(f'Total GB: {totalgb:.2f} GB')
 except FileNotFoundError:
     print("/proc/meminfo not found — are you on Linux?")
 except PermissionError:
